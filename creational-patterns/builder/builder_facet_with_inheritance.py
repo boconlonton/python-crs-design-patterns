653cc142fea4337fd6801d90597950afd87dbb6b
prd-tan-truong <tan.truong@paradox.ai>
2022-07-18 15:02:46 +0700

    Builder - Add Builder Facets (Improvment with Inheritance)

---
+++ creational-patterns/builder/builder_facet_with_inheritance.py
@@ -0,0 +1,53 @@
+"""
+Areas of Improvement: Using Inheritance to comply Open-Closed Principal
+"""
+
+
+class Person:
+    def __init__(self):
+        self.name = None
+        self.position = None
+        self.date_of_birth = None
+
+    def __str__(self):
+        return f'{self.name} born on {self.date_of_birth} ' + \
+               f'works as {self.position}'
+
+    @staticmethod
+    def new():
+        return PersonBuilder()
+
+
+class PersonBuilder:
+    def __init__(self):
+        self.person = Person()
+
+    def build(self):
+        return self.person
+
+
+class PersonInfoBuilder(PersonBuilder):
+    def called(self, name):
+        self.person.name = name
+        return self
+
+
+class PersonJobBuilder(PersonInfoBuilder):
+    def work_as_a(self, position):
+        self.person.position = position
+        return self
+
+
+class PersonBirthDateBuilder(PersonJobBuilder):
+    def born(self, date_of_birth):
+        self.person.date_of_birth = date_of_birth
+        return self
+
+
+pb = PersonBirthDateBuilder()
+me = pb\
+    .called('Dimitry')\
+    .work_as_a('Quant')\
+    .born('1/1/1980')\
+    .build()
+print(me)
