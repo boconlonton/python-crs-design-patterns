33155dfa017091e2ce56c3b972037944a723deac
prd-tan-truong <tan.truong@paradox.ai>
2022-07-18 14:35:30 +0700

    Builder - Update the original example

--- creational-patterns/builder/builder.py
+++ creational-patterns/builder/builder.py
@@ -20,11 +20,11 @@ class HtmlElement:
         lines.append(f'{i}<{self.name}>')
 
         if self.text:
-            i1 = ' ' + ((indent + 1) * self.indent_size)
+            i1 = ' ' * ((indent + 1) * self.indent_size)
             lines.append(f'{i1}{self.text}')
 
         for e in self.elements:
-            lines.append(e.__str(indent+1))
+            lines.append(e._to_string(indent+1))
 
         lines.append(f'{i}</{self.name}>')
         return '\n'.join(lines)
@@ -32,27 +32,45 @@ class HtmlElement:
     def __str__(self):
         return self._to_string(0)
 
-    # @staticmethod
-    # def create(name):
-    #     return Html
+    @staticmethod
+    def create(name):
+        return HtmlBuilder(name)
 
 
 class HtmlBuilder:
-    _root = HtmlElement()
 
     def __init__(self, root_name):
         self.root_name = root_name
-        self._root.name = root_name
+        self._root = HtmlElement(name=root_name)
 
-    # not fluent
     def add_child(self, child_name, child_text):
         self._root.elements.append(
             HtmlElement(child_name, child_text)
         )
+
+    def add_child_fluent(self, child_name, child_text):
+        self._root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+        return self
 
     def __str__(self):
         return str(self._root)
 
 
 root = HtmlBuilder('ul')
-root.add_child()
+root.add_child('li', 'Hello')
+root.add_child('li', 'World')
+print(root)
+
+root_fluent = HtmlBuilder('ul')
+root_fluent\
+    .add_child_fluent('li', 'hello')\
+    .add_child_fluent('li', 'world')
+print(root_fluent)
+
+
+root_2 = HtmlElement.create('ul')
+root_2.add_child('li', 'Hello')
+root_2.add_child('li', 'World2')
+print(root_2)
