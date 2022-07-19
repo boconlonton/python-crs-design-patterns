6259d6326c23291a7e9538a51ebb0c3714a464eb
prd-tan-truong <tan.truong@paradox.ai>
2022-07-19 16:32:07 +0700

    Add - Prototype - lesson/Prototype Factory

---
+++ creational-patterns/prototype/lessons/prototype_factory.py
@@ -0,0 +1,58 @@
+"""
+Usage: When you have a bunch of prototypes
+"""
+
+import copy
+
+
+class Address:
+    def __init__(self, street_address, suite, city):
+        self.suite = suite
+        self.city = city
+        self.street_address = street_address
+
+    def __str__(self):
+        return f'{self.street_address}, Suite #{self.suite}, {self.city}'
+
+
+class Employee:
+    def __init__(self, name, address):
+        self.address = address
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name} works at {self.address}'
+
+
+class EmployeeFactory:
+    main_office_employee = Employee('', Address('123 East Dr', 0, 'London'))
+    aux_office_employee = Employee('', Address('123B East Dr', 0, 'London'))
+
+    @staticmethod
+    def __new_employee(proto, name, suite):
+        result = copy.deepcopy(proto)
+        result.name = name
+        result.address.suite = suite
+        return result
+
+    @staticmethod
+    def new_main_office_employee(name, suite):
+        return EmployeeFactory.__new_employee(
+            EmployeeFactory.main_office_employee,
+            name,
+            suite
+        )
+
+    @staticmethod
+    def new_aux_main_office_employee(name, suite):
+        return EmployeeFactory.__new_employee(
+            EmployeeFactory.aux_office_employee,
+            name,
+            suite
+        )
+
+
+john = EmployeeFactory.new_main_office_employee('John', 101)
+jane = EmployeeFactory.new_aux_main_office_employee('Jane', 500)
+print(john)
+print(jane)
